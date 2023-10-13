9029ab46e39ea4764d41842bc4f693e0cb03e9c2
udara <udaraprabath98@gmail.com>
2023-10-13 11:32:12 +0530

    one

---
+++ inventory/src/Login.js
@@ -0,0 +1,14 @@
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="Page">
+      <h1 className="Heading" >Login</h1>
+      <input className="Input" type="text"/>
+      <input className="Input" type="text"/>
+
+    </div>
+  );
+}
+
+export default Login;
